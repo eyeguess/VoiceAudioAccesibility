f72cb8453bacb40dd9a701b4758cf7769afeb353
iGuEs5 <45712861+iGuEs5@users.noreply.github.com>
2023-11-28 10:19:31 +0100

    Add project files.

---
+++ VoiceAudioAccesibility/Patches/VAA.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using HarmonyLib;
+using Dissonance;
+using UnityEngine.InputSystem;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx.Configuration;
+using BepInEx;
+using BepInEx.Logging;
+using System.Runtime.CompilerServices;
+using UnityEngine.Windows;
+
+namespace VoiceAudioAccesibility.Patches
+{
+
+
+    [HarmonyPatch(typeof(DissonanceComms), "Update")]
+    public static class VAA
+    {
+        private const string modGUID = "eyeguess.VoiceMod";
+        internal static ManualLogSource mls;
+
+        public static void Postfix(ref DissonanceComms __instance)
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            if (Keyboard.current.kKey.isPressed)
+            {
+                __instance.RemoteVoiceVolume = 0f;
+                mls.LogInfo("K button pressed: Everyone has been muted");
+
+            }
+            else if (Keyboard.current.rKey.isPressed)
+            {
+                __instance.RemoteVoiceVolume = 0.1f;
+                mls.LogInfo("R button pressed: Everyone has their voice lowered!");
+
+            }else
+            {
+                __instance.RemoteVoiceVolume = 1f;
+
+            }
+
+        }
+    }
+}
---
+++ VoiceAudioAccesibility/Voice.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using VoiceAudioAccesibility.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace VoiceAudioAccesibility
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class VoiceMod : BaseUnityPlugin
+    {
+
+        private const string modGUID = "eyeguess.VoiceMod";
+        private const string modName = "VoiceAudioAccesibility";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static VoiceMod Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Voice Audio Accesibility is online.");
+            harmony.PatchAll(typeof(VoiceMod));
+            harmony.PatchAll(typeof(VAA));
+
+        }
+
+
+    }
+}
